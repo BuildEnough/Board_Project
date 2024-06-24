abd2ed80df16f3e10d0d34e71fa84d3090803a01
BuildEnough <rhtmaehcl878@naver.com>
2024-06-24 14:34:49 +0900

    h2 Database 설

---
+++ board/src/main/java/com/example/board/MainController.java
@@ -0,0 +1,14 @@
+package com.example.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	@GetMapping("/board")
+	@ResponseBody
+	public String index() {
+		return "index 홈페이지에 오신 것을 환영합니다";
+	}
+}
