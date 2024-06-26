aa695dab2b382cacc5a4519d8d89d220fc02c0e1
BuildEnough <rhtmaehcl878@naver.com>
2024-06-26 19:06:49 +0900

    상세 페이지 작성

--- board/src/main/java/com/example/board/question/QuestionService.java
+++ board/src/main/java/com/example/board/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.example.board.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.example.board.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -16,4 +19,12 @@ public class QuestionService {
 		return this.questionRepository.findAll();
 	}
 
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) { // isPresent(): Optional 객체가 값을 가지고 있다면 true, 아니면 false
+			return question.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
+	}
 }
