6879a850b916bc257abb38baa959301aec8c6e3f
BuildEnough <rhtmaehcl878@naver.com>
2024-06-26 10:56:32 +0900

    기본 웹 페이지 생성

--- board/src/main/java/com/example/board/MainController.java
+++ board/src/main/java/com/example/board/MainController.java
@@ -11,4 +11,9 @@ public class MainController {
 	public String index() {
 		return "index 홈페이지에 오신 것을 환영합니다";
 	}
+	
+	@GetMapping("/") // root URL 설정
+	public String root() {
+		return "redirect:/question/list";
+	}
 }
--- board/src/main/java/com/example/board/question/QuestionController.java
+++ board/src/main/java/com/example/board/question/QuestionController.java
@@ -1,15 +1,23 @@
 package com.example.board.question;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor // final이 붙은 속성을 포함하는 생성자를 자동으로 만들어 줌
 @Controller
 public class QuestionController {
 	
+	private final QuestionRepository questionRepository;
+	
 	@GetMapping("/question/list")
-	@ResponseBody
-	public String list() {
-		return "question list";
+	public String list(Model model) { // Model: 자바 클래스와 템플릿 간의 연결고
+		List<Question> questionList = this.questionRepository.findAll();
+		model.addAttribute("questionList", questionList);
+		return "question_list";
 	}
 }
