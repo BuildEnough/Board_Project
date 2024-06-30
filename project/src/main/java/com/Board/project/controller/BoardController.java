56e6ddc8898da61d4538fabb53913d55882c2842
BuildEnough <rhtmaehcl878@naver.com>
2024-06-30 13:52:55 +0900

    기본 MVC 구현

--- project/src/main/java/com/Board/project/controller/BoardController.java
+++ project/src/main/java/com/Board/project/controller/BoardController.java
@@ -1,13 +1,21 @@
 package com.Board.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BoardController {
 
     @GetMapping("/hi")
-    public String niceToMeetyou() {
+    public String niceToMeetyou(Model model) {
+        model.addAttribute("username", "사용자");
         return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "사용자");
+        return "goodbye";
     }
 }
