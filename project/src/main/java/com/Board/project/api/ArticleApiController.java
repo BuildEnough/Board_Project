76af28b7fcc8fd7985833082c52f865fdcec9e7d
BuildEnough <rhtmaehcl878@naver.com>
2024-07-01 13:51:14 +0900

    REST api 구현

---
+++ project/src/main/java/com/Board/project/api/ArticleApiController.java
@@ -0,0 +1,70 @@
+package com.Board.project.api;
+
+import com.Board.project.dto.ArticleForm;
+import com.Board.project.entity.Article;
+import com.Board.project.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    @GetMapping("/api/articles")
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public Article show(@PathVariable Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/api/articles")
+    public Article create(@RequestBody ArticleForm dto) {
+        Article article = dto.toEntity();
+        return articleRepository.save(article);
+    }
+
+    @PatchMapping("/api/articles/{id}")
+    public ResponseEntity<Article> update(@PathVariable Long id, @RequestBody ArticleForm dto) {
+        // Dto -> Entity
+        Article article = dto.toEntity();
+        log.info("id: {}, article: {}", id, article.toString());
+
+        // 조회
+        Article target = articleRepository.findById(id).orElse(null);
+
+        // 잘못된 요청 처리
+        if (target == null || id != article.getId()) {
+            log.info("잘못된 요청 id: {}, article: {}", id, article.toString());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        // 업데이트, 200 응답
+        target.patch(article);
+        Article updated = articleRepository.save(target);
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Article> delete(@PathVariable Long id) {
+
+        Article target = articleRepository.findById(id).orElse(null);
+
+        if (target == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
+        articleRepository.delete(target);
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+}
