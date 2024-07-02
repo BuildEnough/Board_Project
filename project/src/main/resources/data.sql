a07b13b4d4f84e238d1a6a3cd863c3004bcaf18f
BuildEnough <rhtmaehcl878@naver.com>
2024-07-02 09:43:32 +0900

    댓글 엔티티, 더미 데이터 생성

--- project/src/main/resources/data.sql
+++ project/src/main/resources/data.sql
@@ -1,3 +1,21 @@
 INSERT INTO article(title, content) VALUES('1번', '1111');
 INSERT INTO article(title, content) VALUES('2번', '2222');
 INSERT INTO article(title, content) VALUES('3번', '3333');
+INSERT INTO article(title, content) VALUES('4번', '4444');
+INSERT INTO article(title, content) VALUES('5번', '5555');
+INSERT INTO article(title, content) VALUES('6번', '6666');
+
+-- 4댓글
+INSERT INTO comment(article_id, nickname, body) VALUES(4, '4-1', '4-14-1');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, '4-2', '4-24-2');
+INSERT INTO comment(article_id, nickname, body) VALUES(4, '4-3', '4-34-3');
+
+-- 5댓글
+INSERT INTO comment(article_id, nickname, body) VALUES(5, '5-1', '5-15-1');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, '5-2', '5-25-2');
+INSERT INTO comment(article_id, nickname, body) VALUES(5, '5-3', '5-35-3');
+
+-- 6댓글
+INSERT INTO comment(article_id, nickname, body) VALUES(6, '6-1', '6-16-1');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, '6-2', '6-26-2');
+INSERT INTO comment(article_id, nickname, body) VALUES(6, '6-3', '6-36-3');
